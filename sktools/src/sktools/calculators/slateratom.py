03a9b757677a146fcf48c2f4c9a6d06c4605e481
Alexander Maryewski <alex.maryewski@gmail.com>
2026-09-09 17:52:14 +0200

    remove spurious print statement

--- sktools/src/sktools/calculators/slateratom.py
+++ sktools/src/sktools/calculators/slateratom.py
@@ -74,7 +74,6 @@ class SlaterAtomSettings(sc.ClassDict):
         self.maxscfiter = maxscfiter
         self.mixer = mixer
         self.mixfactor = mixfactor
-        print(self.__dict__)
 
     @classmethod
     def fromhsd(cls, root, query):
